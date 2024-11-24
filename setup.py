673b1aa9227bd3d22154c81151d14341d98661cd
unknown <20070866@vnu.edu.vn>
2024-11-24 22:13:29 +0700

    continue

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='balance_bot',
-      version='0.0.3',
-      install_requires=['gym',
-                        'pybullet',
-                        'baselines']
-)
